6cb5f32787f6e8c0dcf10e22e4069b732a2c1b91
Turtle2sday <kaiknecht20@gmail.com>
2025-02-19 14:48:26 -0800

    AI is 99% done
    but he cant see through ladders

--- Assets/Scripts/AstronautAI.cs
+++ Assets/Scripts/AstronautAI.cs
@@ -1,34 +1,29 @@
 using UnityEngine;
 using System.Collections;
-using Unity.VisualScripting;
 
 public class AstronautAI : MonoBehaviour
 {
-
     private enum AstronautState { Patrolling, Chasing, Stunned, Scared, Attacking }
-
-    AstronautState state;
+    private AstronautState state;
 
     private bool stateComplete;
-    [SerializeField] GameObject player;
-    [SerializeField] GameObject rayOrigin;
-    [SerializeField] Rigidbody2D rb;
-    [SerializeField] private LayerMask groundLayer;
-
-    private float chaseTime;
-    private float moveSpeed;
-    private bool chaseTimerOn = false;
     private bool stunned;
-    private bool chasing = false;
-    private float velocityXSmooth = 0f;
+    private bool chasing;
+    private bool isFacingRight;
+    private bool playerInView;
 
-    private bool isFacingRight = false;
-    [SerializeField] bool playerInView = false;
+    private float moveSpeed;
+    private float velocityXSmooth = 0f;
+    private float timeToHide = 999999 * 999999f;
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject rayOrigin;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private LayerMask groundLayer;
 
     void Start()
     {
         state = AstronautState.Patrolling;
-
     }
 
     void Update()
@@ -36,67 +31,31 @@ public class AstronautAI : MonoBehaviour
         SpawnRaycast();
         UpdateState();
 
-        if (playerInView && state == AstronautState.Patrolling || stunned)
-        {
+        if ((playerInView && state == AstronautState.Patrolling) || stunned)
             StartCoroutine(StandStill());
-        }
-        if (chasing && !playerInView && !chaseTimerOn)
-        {
-            chaseTime = Time.time + 3f;
-            chaseTimerOn = true;
-        }
-
-        if (Time.time > chaseTime)
-        {
-            if (!playerInView)
-            {
-                chasing = false;
-            }
-            chaseTimerOn = false;
-        }
 
         if (stateComplete)
-        {
             SelectState();
-        }
 
         if (!stunned && !chasing)
-        {
             FlipCheck();
-        }
-
-        //Debug.Log(TouchingPlayer());
-        Debug.Log(state);
-        //Debug.Log(isFacingRight);
+        if (playerInView)
+            timeToHide = Time.time + 3f;
     }
 
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-
         if (state == AstronautState.Patrolling || state == AstronautState.Chasing)
         {
             int direction = isFacingRight ? 1 : -1;
-            float newVelocityX;
-            if (state == AstronautState.Chasing)
-            {
-                
-                float smoothTime = 0.2f;
-                float targetVelocityX = direction * moveSpeed;
-                newVelocityX = Mathf.SmoothDamp(rb.linearVelocity.x, targetVelocityX, ref velocityXSmooth, smoothTime);
+            float targetVelocityX = direction * moveSpeed;
+            float smoothTime = state == AstronautState.Chasing ? 0.2f : 0f;
 
-                Vector2 playerDirection = (new Vector2(player.transform.position.x, 0) - new Vector2(transform.position.x, 0)).normalized;
-                if (playerDirection.x > 0 && !isFacingRight || playerDirection.x < 0 && isFacingRight)
-                {
-                    Flip();
-                }
-            }
-            else
-            {
-                newVelocityX = moveSpeed * direction;
-            }
-            
+            float newVelocityX = Mathf.SmoothDamp(rb.linearVelocity.x, targetVelocityX, ref velocityXSmooth, smoothTime);
             rb.linearVelocity = new Vector2(newVelocityX, rb.linearVelocity.y);
+
+            if (state == AstronautState.Chasing)
+                AdjustFacingDirection();
         }
     }
 
@@ -104,156 +63,104 @@ public class AstronautAI : MonoBehaviour
     {
         switch (state)
         {
-            case AstronautState.Stunned:
-                UpdateStun();
-                break;
-            case AstronautState.Scared:
-                UpdateScared();
-                break;
-            case AstronautState.Attacking:
-                UpdateAttack();
-                break;
-            case AstronautState.Chasing:
-                UpdateChase();
-                break;
-            case AstronautState.Patrolling:
-                UpdatePatrol();
-                break;
-
+            case AstronautState.Stunned: UpdateStun(); break;
+            case AstronautState.Scared: UpdateScared(); break;
+            case AstronautState.Attacking: UpdateAttack(); break;
+            case AstronautState.Chasing: UpdateChase(); break;
+            case AstronautState.Patrolling: UpdatePatrol(); break;
         }
-        
     }
+
     void SelectState()
     {
         stateComplete = false;
+
         if (stunned)
         {
             state = AstronautState.Stunned;
-            StartStun();
+            return;
+        }
+
+        if (TouchingPlayer())
+        {
+            state = AstronautState.Attacking;
+        }
+        else if (chasing)
+        {
+            state = AstronautState.Chasing;
+        }
+        else if (playerInView)
+        {
+            if (state == AstronautState.Patrolling)
+                state = AstronautState.Scared;
         }
         else
         {
-            if (chasing)
-            {
-                state = AstronautState.Chasing;
-                StartChase();
-
-                if (TouchingPlayer())
-                {
-                    state = AstronautState.Attacking;
-                    StartAttack();
-                }
-            }
-            else if (playerInView && state == AstronautState.Patrolling && !chasing)
-            {
-                state = AstronautState.Scared;
-                StartScared();
-            }
-            else
-            {
-                if (!chasing)
-                {
-                    state = AstronautState.Patrolling;
-                    StartPatrol();
-                }
-                
-            }
+            state = AstronautState.Patrolling;
         }
     }
 
     void UpdateStun()
     {
         rb.linearVelocity = Vector2.zero;
-        StandStill();
+        StartCoroutine(StandStill());
     }
+
     void UpdateScared()
     {
         rb.linearVelocity = Vector2.zero;
-        StandStill();
+        StartCoroutine(StandStill());
     }
 
     IEnumerator StandStill()
     {
         yield return new WaitForSeconds(1f);
-        if (playerInView)
-        {
-            chasing = true;
-        }
+        if (playerInView) chasing = true;
         stunned = false;
         stateComplete = true;
-        
     }
+
     void UpdateAttack()
     {
-        ChargeAttack();
+        StartCoroutine(ChargeAttack());
     }
+
     IEnumerator ChargeAttack()
     {
         rb.linearVelocity = Vector2.zero;
-        yield return new WaitForSeconds(.2f);
+        yield return new WaitForSeconds(0.5f);
+
         if (TouchingPlayer())
         {
-            Debug.LogWarning("playerDied");
-            
+            // Player hit logic here
         }
-        else
-        {
-            Debug.LogWarning("IMissedFuckYou");
-        }
+
         chasing = true;
         stateComplete = true;
     }
+
     void UpdateChase()
     {
-        moveSpeed = 6f;
-
-        if (!chasing || TouchingPlayer())
-        {
+        moveSpeed = 8f;
+        if (!chasing || TouchingPlayer() || Time.time > timeToHide)
+            chasing = false;
             stateComplete = true;
-        }
     }
     void UpdatePatrol()
     {
-        moveSpeed = 3f;
         WallCheck();
+        moveSpeed = 4f;
         if (playerInView || stunned)
-        {
             stateComplete = true;
-        }
-        
     }
 
-    void StartStun()
+    void FlipCheck()
     {
-        //animation;
-    }
-    void StartScared()
-    {
-        //animation;
-    }
-    void StartAttack()
-    {
-        //animation;
-    }
-    void StartChase()
-    {
-        //animation;
-    }
-    void StartPatrol()
-    {
-        //animation;
-    }
-
-
-    private void FlipCheck()
-    {
-        if (isFacingRight && rb.linearVelocity.x < 0f || !isFacingRight && rb.linearVelocity.x > 0f)
-        {
+        if ((isFacingRight && rb.linearVelocity.x < 0) || (!isFacingRight && rb.linearVelocity.x > 0))
             Flip();
-        }
     }
 
-    private void Flip()
+    void Flip()
     {
         isFacingRight = !isFacingRight;
         Vector3 localScale = transform.localScale;
@@ -261,24 +168,23 @@ public class AstronautAI : MonoBehaviour
         transform.localScale = localScale;
     }
 
-    private void WallCheck()
+    void WallCheck()
     {
         if (TouchingWall())
-        {
             Flip();
-        }
     }
 
     private bool TouchingWall()
     {
-        return Physics2D.OverlapCircle(rayOrigin.transform.position, 1f, groundLayer);
-
+        return Physics2D.OverlapCircle(rayOrigin.transform.position, 0.55f, groundLayer);
     }
+
     private bool TouchingPlayer()
     {
         Collider2D collider = Physics2D.OverlapCircle(rayOrigin.transform.position, 1f);
         return collider != null && collider.gameObject.name == "Player";
     }
+
     private void SpawnRaycast()
     {
         Vector2 direction = (player.transform.position - rayOrigin.transform.position).normalized;
@@ -294,41 +200,27 @@ public class AstronautAI : MonoBehaviour
             {
                 if (!chasing)
                 {
-                    if (isFacingRight)
-                    {
-                        if (angle > 40)
-                        {
-                            playerInView = true;
-                        }
-                        else
-                        {
-                            playerInView = false;
-                        }
-                    }
-                    if (!isFacingRight)
-                    {
-                        if (angle < 140)
-                        {
-                            playerInView = true;
-                        }
-                        else
-                        {
-                            playerInView = false;
-                        }
-                    }
+                    playerInView = isFacingRight ? angle > 120 : angle < 60;
                 }
                 else
                 {
                     playerInView = true;
                 }
             }
+            else
+            {
+                if (hit.collider.gameObject.layer != 6)
+                playerInView = false;
+            }
         }
     }
 
-
-    
-    
-        
+    private void AdjustFacingDirection()
+    {
+        Vector2 playerDirection = (new Vector2(player.transform.position.x, 0) - new Vector2(transform.position.x, 0)).normalized;
+        if ((playerDirection.x > 0 && !isFacingRight) || (playerDirection.x < 0 && isFacingRight))
+            Flip();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
